e8f7b4de4b47fc24f0c8db09dce2fff43e0794aa
Hồ Hoài Phong <longtam20015@gmail.com>
2023-06-17 17:38:50 +0700

    Management state

---
+++ src/components/MyComponent.js
@@ -0,0 +1,21 @@
+// class component
+// function component
+import React from "react";
+class MyComponent extends React.Component {
+  state = {
+    name: "HaryPhamDev",
+    address: "Hoi Dan IT",
+    age: 26,
+  };
+
+  // JSX
+  render() {
+    return (
+      <div>
+        My name is {this.state.name} and I'm from {this.state.address}
+      </div>
+    );
+  }
+}
+
+export default MyComponent;
