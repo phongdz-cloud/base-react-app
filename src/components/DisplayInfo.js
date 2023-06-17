4797248e0143cae6c0ad34ae29dc27b896db4da8
Hồ Hoài Phong <longtam20015@gmail.com>
2023-06-17 20:19:36 +0700

    Array in react

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -3,12 +3,27 @@ import React from "react";
 class DisplayInfo extends React.Component {
   render() {
     // desctructuring array/object
-    const { age, name } = this.props;
+    const { listUsers } = this.props;
     // props => viết tắt property
     return (
       <div>
+        {listUsers.map((user) => {
+          return (
+            <div key={user.id}>
+              <div>My name's {user.name} </div>
+              <div>My age's {user.age} </div>
+              <hr />
+            </div>
+          );
+        })}
+        {/* <div>My name's {age}</div>
+        <div>My age's {name}</div>
+        <hr />
         <div>My name's {age}</div>
         <div>My age's {name}</div>
+        <hr />
+        <div>My name's {age}</div>
+        <div>My age's {name}</div> */}
       </div>
     );
   }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -4,17 +4,34 @@ import React from "react";
 import UserInfo from "./UserInput";
 import DisplayInfo from "./DisplayInfo";
 class MyComponent extends React.Component {
-  // JSX
-  render() {
-    const myInfo = ["ab", "c", "c"];
+  state = {
+    listUsers: [
+      {
+        id: 1,
+        name: "Hoi Dan IT",
+        age: "26",
+      },
+      {
+        id: 2,
+        name: "Hoai Phong",
+        age: "24",
+      },
+      {
+        id: 3,
+        name: "Anh Nhi",
+        age: "23",
+      },
+    ],
+  };
 
+  // JSX
+  // DRY: don't repeat youseft
+  render() {
     return (
       <div>
         <UserInfo />
         <br /> <br />
-        <DisplayInfo name="Hoi Dan IT" age="30" />
-        <hr />
-        <DisplayInfo name="Phong" age={26} myInfo={myInfo} />
+        <DisplayInfo listUsers={this.state.listUsers} />
       </div>
     );
   }
