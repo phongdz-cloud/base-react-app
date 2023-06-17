48a8febd5d11c48553aad9d910f0cc6998c12a17
Hồ Hoài Phong <longtam20015@gmail.com>
2023-06-17 18:01:26 +0700

    xử lý event cho react

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -8,11 +8,22 @@ class MyComponent extends React.Component {
     age: 26,
   };
 
+  handleClick(event) {
+    console.log(">> Click me my button");
+    console.log(" My name is ", this.state.name);
+  }
+
+  handleOnMoverOver(event) {
+    console.log(event);
+  }
+
   // JSX
   render() {
     return (
       <div>
         My name is {this.state.name} and I'm from {this.state.address}
+        <button onMouseOver={this.handleOnMoverOver}>Hover me</button>
+        <button onClick={this.handleClick}>Click me</button>
       </div>
     );
   }
