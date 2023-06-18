11ee477abf1339ea712412a59bd60183f85a284a
Hồ Hoài Phong <longtam20015@gmail.com>
2023-06-18 13:24:00 +0700

    react-router-dom và react bootstrap và bootstrap

--- src/App.js
+++ src/App.js
@@ -1,35 +1,6 @@
 import "./App.scss";
-import MyComponent from "./components/MyComponent";
-import React from "react";
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <MyComponent />
-      </div>
-    );
-  }
-}
-
-// const App = () => {
-//   const count = useSelector((state) => state.counter.count);
-//   const dispatch = useDispatch();
-
-//   return (
-//     <MyComponent />
-//     // <div className="App">
-//     //   <header className="App-header">
-//     //     <img src={logo} className="App-logo" alt="logo" />
-//     //     <p>
-//     //       Edit <code>src/App.js</code> and save to reload.
-//     //     </p>
-//     //     <div>Count = {count}</div>
-//     //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//     //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//     //   </header>
-//     // </div>
-//   );
-// };
+const App = () => {
+  return <div className="app-container">hello world</div>;
+};
 
 export default App;
