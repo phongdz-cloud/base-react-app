d5fb342114590a0859b132a24dedd1bac4fbccab
Hồ Hoài Phong <longtam20015@gmail.com>
2023-06-18 14:50:07 +0700

    Nav link cho home

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,9 @@ const Header = () => {
   return (
     <Navbar bg="light" expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Hỏi Dân IT</Navbar.Brand>
+        <NavLink to="/" className="navbar-brand">
+          Hỏi Dân IT
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
@@ -22,15 +24,12 @@ const Header = () => {
             <NavLink to="/admins" className="nav-link">
               Admin
             </NavLink>
-            {/* <Nav.Link href="/">Home</Nav.Link> 
-            <Nav.Link href="/users">User</Nav.Link>
-            <Nav.Link href="/admins">Admin</Nav.Link> */}
           </Nav>
           <Nav>
             <NavDropdown title="Settings" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Log out</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>
+              <NavDropdown.Item>Log in</NavDropdown.Item>
+              <NavDropdown.Item>Log out</NavDropdown.Item>
+              <NavDropdown.Item>Profile</NavDropdown.Item>
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
---
+++ src/components/Home/HomePage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const HomePage = (props) => {
+  return <div className="homepage-container">home page component</div>;
+};
+
+export default HomePage;
