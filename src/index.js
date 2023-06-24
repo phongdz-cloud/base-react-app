4d9961533b8eaf47518ab6146397f0f88981995b
Hồ Hoài Phong <longtam20015@gmail.com>
2023-06-24 21:48:46 +0700

    Persist gate

--- src/index.js
+++ src/index.js
@@ -3,16 +3,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 import reportWebVitals from "./reportWebVitals";
 import Layout from "./Layout";
 import "nprogress/nprogress.css";
+import { PersistGate } from "redux-persist/integration/react";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <Layout />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <Layout />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>
 );
 
