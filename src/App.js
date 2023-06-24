8abe92803c30141eabfb330718a251e5e4c7c1c4
Hồ Hoài Phong <longtam20015@gmail.com>
2023-06-24 13:45:20 +0700

    reduce

--- src/App.js
+++ src/App.js
@@ -1,35 +1,36 @@
 import "./App.css";
-import MyComponent from "./components/MyComponent";
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { increaseCounter, decreaseCounter } from "./redux/action/counterAction";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <MyComponent />
-      </div>
-    );
-  }
-}
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <MyComponent />
+//       </div>
+//     );
+//   }
+// }
 
-// const App = () => {
-//   const count = useSelector((state) => state.counter.count);
-//   const dispatch = useDispatch();
+const App = () => {
+  const count = useSelector((state) => state.counter.count);
+  // stateRedux => call Reducer => state's reducer
+  // state redux changes => react changes
+  const dispatch = useDispatch();
 
-//   return (
-//     <MyComponent />
-//     // <div className="App">
-//     //   <header className="App-header">
-//     //     <img src={logo} className="App-logo" alt="logo" />
-//     //     <p>
-//     //       Edit <code>src/App.js</code> and save to reload.
-//     //     </p>
-//     //     <div>Count = {count}</div>
-//     //     <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//     //     <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//     //   </header>
-//     // </div>
-//   );
-// };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div>Count = {count}</div>
+        <button onClick={() => dispatch(increaseCounter())}>Increase</button>
+        <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
+      </header>
+    </div>
+  );
+};
 
 export default App;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,11 @@
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import rootReducer from './reducer/rootReducer';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducer/rootReducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
