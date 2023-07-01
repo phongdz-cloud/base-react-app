8cbc697d947735165e57a17c2f9be674cbd1e81c
Hồ Hoài Phong <longtam20015@gmail.com>
2023-07-01 15:53:59 +0700

    ref in react

--- src/components/User/Content/RightContent.js
+++ src/components/User/Content/RightContent.js
@@ -1,11 +1,39 @@
 import React from "react";
 import CountDown from "./CountDown";
-
+import { useRef } from "react";
 const RightContent = (props) => {
+  const refDiv = useRef([]);
   const { dataQuiz } = props;
-  console.log("dataQuiz: ", dataQuiz);
+
   const onTimeUp = () => {
     props.handleFinishQuiz();
+  };
+
+  const getClassQuestion = (question, index) => {
+    console.log(index, question);
+    // check answered
+    if (question && question.answer.length > 0) {
+      let isUnAnswered = question.answer.some((a) => a.isSelected);
+      if (isUnAnswered) return "question selected";
+    }
+    return "question";
+  };
+
+  const handleClickQuestion = (question, index) => {
+    props.setIndex(index);
+    if (refDiv.current) {
+      refDiv.current.forEach((item) => {
+        if (item && item.className === "question clicked") {
+          item.className = "question";
+        }
+      });
+    }
+
+    if (question && question.answer.length > 0) {
+      let isUnAnswered = question.answer.some((a) => a.isSelected);
+      if (isUnAnswered) return;
+    }
+    refDiv.current[index].className = "question clicked";
   };
   return (
     <>
@@ -17,7 +45,12 @@ const RightContent = (props) => {
           dataQuiz.length > 0 &&
           dataQuiz.map((item, index) => {
             return (
-              <div key={`question-abc-${index}`} className="question">
+              <div
+                key={`question-abc-${index}`}
+                className={getClassQuestion(item, index)}
+                onClick={() => handleClickQuestion(item, index)}
+                ref={(element) => (refDiv.current[index] = element)}
+              >
                 {index + 1}
               </div>
             );
